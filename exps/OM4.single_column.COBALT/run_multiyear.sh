8716ce0939905d5948eef333a15f4233ffd3e6d2
jbrzensk <jbrzensk@gmail.com>
2024-09-24 15:33:19 -0700

    added two parameters to parallel and multiyear

--- exps/OM4.single_column.COBALT/run_multiyear.sh
+++ exps/OM4.single_column.COBALT/run_multiyear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # THIS SCRIPT ALLOWS TO LOOP THE 1D COLUNM RUN OF THE ONLINE 
-# COBALT-FEISTY OVER A GIVEN NUMBER OF YEARS <NUM_ITERATIONS>
+# COBALT-FEISTY OVER A GIVEN NUMBER OF YEARS <NUM_YEARS>
 # WITH A SPECIFIC PARAMETER VALUE FOR NONFMORT.
 # 
 # CONTACT: REMY DENECHERE <RDENECHERE@UCSD.EDU>
@@ -27,8 +27,12 @@
 ###############################################################################
 # CHECK IF THE CORRECT NUMBER OF ARGUMENTS ARE PROVIDED
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <Unique Name> <number of iterations (years)> <nonFmort Value> <cpu_core>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <Location Name> <number of (years)> <cpu_core> <nonFmort Value> <encounter_val> <k value>"
+    echo "nonFmort: fish mortality"
+    echo "encounter val: coefficient of encounters [ 30 - 110 ]"
+    echo "k value: exponent, k==1 function of 2nd type, k>1, function of thrid type"
+    echo ""
     exit 1
 fi
 
@@ -52,20 +56,22 @@ fi
 
 ###############################################################################
 # ASSIGN ARGUMENTS TO VARIABLES
-UNIQUE_NAME="$1"
-NUM_ITERATIONS="$2"
-NONFMORT="$3"
-CPU_CORE="$4"
+LOC_NAME="$1"
+NUM_YEARS="$2"
+CPU_CORE="$3"
+NONFMORT="$4"
+ENCOUNTER="$5"
+K_EXP="$6"
 
 ###############################################################################
 # SET HOME DIRECTORY
 HOME_DIR=$(pwd)
 
 # This variable will be set by a overlord
-UNIQUE_ID=10
+UNIQUE_ID="${LOC_NAME}_CPU_${CPU_CORE}_nonFmort_${NONFMORT}_encounter_${ENCOUNTER}_k_${K_EXP}"
 
 # SETUP FOLDER FOR PARALLES RUNS
-LONG_NAME="nonFmort_${NONFMORT}"
+LONG_NAME="nonFmort_${NONFMORT}_encounter_${ENCOUNTER}_k_${K_EXP}"
 WORK_DIR="${SCRATCH_DIR}/${LONG_NAME}"
 if [ -d "$WORK_DIR" ]; then
     echo "$WORK_DIR" exists 
@@ -91,11 +97,25 @@ cp -rf * "${WORK_DIR}"
 cd "${WORK_DIR}"
 
 # MAKE THE RUNS DIRECTORY
-mkdir RUNS
+if [ -d "${WORK_DIR}/RUNS" ]; then
+    echo "RUNS Directory exists."
+else
+    echo "RUNS Directory does not exist, making it..."
+    mkdir RUNS
+fi
+#mkdir RUNS
 
 # EDIT THE INPUT FILE FOR nonFmort
 NEW_LINE="nonFmort = ${NONFMORT}"
 sed -i "/nonFmort/c\\ ${NEW_LINE}" input.nml
+
+# EDIT THE INPUT FILE FOR nonFmort
+NEW_LINE="a_enc = ${ENCOUNTER}"
+sed -i "/a_enc/c\\ ${NEW_LINE}" input.nml
+
+# EDIT THE INPUT FILE FOR nonFmort
+NEW_LINE="k_fct_tp = ${K_EXP}"
+sed -i "/k_fct_tp/c\\ ${NEW_LINE}" input.nml
 
 cd INPUT/
 /project/rdenechere/CEFI-regional-MOM6-FEISTY/link_database.sh
@@ -113,7 +133,7 @@ mpiexec --cpu-set "${CPU_CORE}" --bind-to core --report-bindings -np 1 ./MOM6SIS
 ####################################################
 # MOVE THE DATA TO A NEW FOLDER: 
 ####################################################
-FOLDER_SAVE_LOC="RUNS/${UNIQUE_NAME}"
+FOLDER_SAVE_LOC="RUNS/${LOC_NAME}"
 
 if [ -d "$FOLDER_SAVE_LOC" ]; then
     echo "$FOLDER_SAVE_LOC" exist 
@@ -122,7 +142,7 @@ else
 fi
 
 # SAVE YEAR 1!!
-YEAR_FOLDER_PATH="$FOLDER_SAVE_LOC/${UNIQUE_NAME}_nonFmort${NONFMORT}_yr_1"
+YEAR_FOLDER_PATH="$FOLDER_SAVE_LOC/${LONG_NAME}_yr_1"
 if [ -d "$YEAR_FOLDER_PATH" ]; then 
     rm -rf "$YEAR_FOLDER_PATH"/*
 else 
@@ -137,10 +157,10 @@ yes | cp -i *feisty*.nc "$YEAR_FOLDER_PATH"
 sed -i "s/input_filename = 'n'/input_filename = 'r'/g" input.nml
 yes | cp -i RESTART/*.nc INPUT/
 
-for ((i=2; i<=NUM_ITERATIONS; i++))
+for ((i=2; i<=NUM_YEARS; i++))
 do
     # Create a new folder to save the data of that year: 
-    YEAR_FOLDER_PATH="$FOLDER_SAVE_LOC/${UNIQUE_NAME}_nonFmort${NONFMORT}_yr_${i}"
+    YEAR_FOLDER_PATH="$FOLDER_SAVE_LOC/${LONG_NAME}_yr_${i}"
     if [ -d "$YEAR_FOLDER_PATH" ]; then 
         rm -rf "$YEAR_FOLDER_PATH"/*
     else 
@@ -149,7 +169,7 @@ do
 
     # Run the model and save the outputs in $folder_save_exp
     mpiexec --cpu-set "${CPU_CORE}" --bind-to core --report-bindings -np 1  ./MOM6SIS2 |& tee stdout."${UNIQUE_ID}".env
-    yes | cp -i *feisty*.nc "$YEAR_FOLDER_PATH"/
+    #yes | cp -i *feisty*.nc "$YEAR_FOLDER_PATH"/
 
     # get restart files: 
     yes | cp -i RESTART/*.nc INPUT/
@@ -158,7 +178,7 @@ done
 ###############################################################################
 # End the experiment: ---------------------------------------------------------
 ## save the restart files of last year for potential resimulation: 
-FOLDER_SAVE_RESTART="${UNIQUE_NAME}_restart_nonFmort${NONFMORT}_yr_${NUM_ITERATIONS}"
+FOLDER_SAVE_RESTART="${LONG_NAME}_yr_${NUM_YEARS}"
 
 mkdir "$FOLDER_SAVE_RESTART"
 
@@ -172,10 +192,10 @@ sed -i "s/input_filename = 'r'/input_filename = 'n'/g" input.nml
 # SAVE EVERYTHING IN THE SAVE DIRECTORY
 ############################################
 cp -r RUNS/* "$SAVE_DIR"
-cp -r "$FOLDER_SAVE_RESTART" "${SAVE_DIR}/${UNIQUE_NAME}"
+cp -r "$FOLDER_SAVE_RESTART" "${SAVE_DIR}/${LOC_NAME}"
 
 cd "$HOME_DIR"
 # REMOVE WORKING DIRECTORY AND FOLDERS, ETC...
-# rm -r "$WORK_DIR"
+rm -r "$WORK_DIR"
 
 echo "Simulation done!"
