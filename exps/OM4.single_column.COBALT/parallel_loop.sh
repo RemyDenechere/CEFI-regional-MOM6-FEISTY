9868bb2217321654c5903f39dc2d00013fb41243
jbrzensk <jbrzensk@gmail.com>
2024-09-24 15:33:19 -0700

    added two parameters to parallel and multiyear

--- exps/OM4.single_column.COBALT/parallel_loop.sh
+++ exps/OM4.single_column.COBALT/parallel_loop.sh
@@ -46,7 +46,7 @@
 export CEFI_DATASET_LOC=/project/rdenechere/CEFI-regional-MOM6-FEISTY/exps/datasets/
 export CEFI_EXECUTABLE_LOC=/project/rdenechere/CEFI-regional-MOM6-FEISTY/builds/build/rockfish-linux-gnu/ocean_ice/prod/MOM6SIS2
 export SCRATCH_DIR=/scratch/
-export SAVE_DIR=/project/rdenechere/COBALT_output/parallel/
+#export SAVE_DIR=/project/rdenechere/COBALT_output/parallel/
 #
 ########################################################################################
 # FUNCTIONS USED BY SCRIPT
@@ -81,8 +81,16 @@ isnum_Case() { case ${1#[-+]} in ''|.|*[!0-9.]*|*.*.*) return 1;; esac ;}
 ########################################################################################
 
 # CHECK IF THE CORRECT NUMBER OF ARGUMENTS ARE PROVIDED
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <LocationName> <number of years> <number of discretizations> <nonFmort Starting Value> <nonFmort Ending Value>"
+if [ "$#" -ne 9 ] && [ "$#" -ne 11 ]; then
+    echo "Usage: $0 <LocationName> <number of years> 
+    <number of fmort discretizations> <nonFmort Starting Value> <nonFmort Ending Value>
+    <Number of encounter_coefficient discretizations> <encouter_coef start value> <encounter_coef end value>
+    <K discretizations> <K start value> <K end value>"
+    echo "If K is equal to 1, this is type 2 function."
+    echo "If K is >= 1, this is a type 3 function."
+    echo ""
+    echo "If <K discretizations> == 1, then K start value assumed == 1"
+    echo ""
     exit 1
 fi
 
@@ -112,78 +120,186 @@ fi
 # ASSIGN ARGUMENTS TO VARIABLES
 LOCATION_NAME="$1"
 NUM_YEARS="$2"
-NUM_PARALLEL_DISC="$3"
+FMORT_DISC="$3"
 FMORT_START="$4"
 FMORT_END="$5"
+ENCOUNTER_DISC="$6"
+ENCOUNTER_START="$7"
+ENCOUNTER_END="$8"
+K_DISC="$9"
+if [ "$#" == 11 ]; then
+	K_START="${10}"
+	K_END="${11}"
+fi
 
 ###############################################################################
-# CHECK TO MAKE SURE NUM_PARALLEL_DISC, THE NUMBER OF PARALLEL LOOPS TO
+# CHECK TO MAKE SURE FMORT_DISC, THE NUMBER OF PARALLEL LOOPS TO
 # RUN,  IS A NUMBER, >= 1
-if [[ "$NUM_PARALLEL_DISC" =~ ^[0-9]+$ ]] && (( NUM_PARALLEL_DISC >= 1 )); then
-    echo "Running run_multiyear.sh $NUM_PARALLEL_DISC times..."
+if [[ "$FMORT_DISC" =~ ^[0-9]+$ ]] && (( FMORT_DISC >= 1 )); then
+    echo "Running run_multiyear.sh $FMORT_DISC times..."
 else
-    echo "$NUM_PARALLEL_DISC is not a valid number or is less than 1, quitting..."
+    echo "$FMORT_DISC is not a valid number or is less than 1, quitting..."
     exit 1
 fi
 
 ###############################################################################
-# CHECK IF THE ARGUMENT IS A NUMBER BETWEEN 0 AND 1 (INCLUSIVE)
+# CHECK IF THE FMORT ARGUMENT IS A NUMBER BETWEEN 0 AND 1 (INCLUSIVE)
 echo "Checking nonFmort start and end values..."
-if [[ "$FMORT_START" =~ ^0(\.[0-9]+)?$|^1(\.0+)?$ ]]; then
-    echo "Fmort start value $FMORT_START is between 0 and 1 (inclusive)"
+if (( $FMORT_DISC > 1 )); then
+    if [[ "$FMORT_START" =~ ^0(\.[0-9]+)?$|^1(\.0+)?$ ]]; then
+        echo "Fmort start value $FMORT_START is between 0 and 1 (inclusive)"
+    else
+        echo "FMort start value $FMORT_START is not between 0 and 1 (inclusive), exiting..."
+        exit 1
+    fi
+
+    if [[ "$FMORT_END" =~ ^0(\.[0-9]+)?$|^1(\.0+)?$ ]]; then
+        echo "Fmort end value $FMORT_END is between 0 and 1 (inclusive)"
+    else
+        echo "FMort end value $FMORT_END is not between 0 and 1 (inclusive), exiting..."
+        exit 1
+    fi
+fi
+echo ""
+
+###############################################################################
+# CHECK IF THE ENCOUNTER ARGUMENT IS A NUMBER BETWEEN 0 AND 1 (INCLUSIVE)
+echo "Checking encounter start and end values..."
+    if (( $ENCOUNTER_DISC > 1  )); then
+    if (( "$ENCOUNTER_START" >= 30 && "$ENCOUNTER_START" <= 110 )) ; then
+        echo "Encounter start value $ENCOUNTER_START is between 30 and 110 (inclusive)"
+    else
+        echo "Encounter start value $ENCOUNTER_START is not between 30 and 110 (inclusive), exiting..."
+        exit 1
+    fi
+
+    if (( "$ENCOUNTER_END" > "$ENCOUNTER_START" && "$ENCOUNTER_END" <= 110 )); then
+        echo "Encounter end value $ENCOUNTER_END is between $ENCOUNTER_START  and 110 (inclusive)"
+    else
+        echo "Encounter end value $ENCOUNTER_END is not between $ENCOUNTER_START and 110 (inclusive), exiting..."
+        exit 1
+    fi
+fi
+echo ""
+
+###############################################################################
+# CHECK IF THE K Exponent ARGUMENT IS A NUMBER BETWEEN 0 AND 1 (INCLUSIVE)
+if (( "$K_DISC" > 1 )); then
+    echo "Checking K start and end values..."
+    #if (( "$K_START" >= 1 && "$K_START" <= 20 )); then
+    if (( $(echo "$K_START >= 1.0" | bc -l) )); then
+        echo "K start value $K_START is >= 1 (inclusive)"
+    else
+        echo "K start value $K_START is not between 1 and 20 (inclusive), exiting..."
+        exit 1
+    fi
+
+    if (( $(echo "$K_END > $K_START" | bc -l) )); then
+        echo "K end value $K_END is between $K_START  and 20 (inclusive)"
+    else
+        echo "K end value $K_END is not between $K_START and 20 (inclusive), exiting..."
+        exit 1
+    fi
 else
-    echo "FMort start value $FMORT_START is not between 0 and 1 (inclusive), exiting..."
-    exit 1
+    echo "K value of 1 used. Function type 2"
 fi
 
-if [[ "$FMORT_END" =~ ^0(\.[0-9]+)?$|^1(\.0+)?$ ]]; then
-    echo "Fmort end value $FMORT_END is between 0 and 1 (inclusive)"
-else
-    echo "FMort end value $FMORT_END is not between 0 and 1 (inclusive), exiting..."
-    exit 1
+echo ""
+
+###############################################################################
+###############################################################################
+# GENERATE THE ARRAY OF VALUES TO TRY FOR PARAMETER
+# Generate array
+if (( $FMORT_DISC > 1 )); then
+    NONFMORT_ARRAY=($(generate_array $FMORT_START $FMORT_END $FMORT_DISC))
+    # find last index
+    last_index=$((${#NONFMORT_ARRAY[@]} - 1))
+    # Ensure the first and last values are the two desired values
+    NONFMORT_ARRAY[0]="$FMORT_START"
+    NONFMORT_ARRAY[$last_index]=$FMORT_END
+else 
+    NONFMORT_ARRAY[0]="$FMORT_START"
 fi
 
+echo "nonFmort will use the following values: ${NONFMORT_ARRAY[@]}"
 echo ""
 
 ###############################################################################
 # GENERATE THE ARRAY OF VALUES TO TRY FOR PARAMETER
 # Generate array
-NONFMORT_ARRAY=($(generate_array $FMORT_START $FMORT_END $NUM_PARALLEL_DISC))
-# find last index
-last_index=$((${#NONFMORT_ARRAY[@]} - 1))
-# Ensure the first and last values are the two desired values
-NONFMORT_ARRAY[0]="$FMORT_START"
-NONFMORT_ARRAY[$last_index]=$FMORT_END
+if (( $ENCOUNTER_DISC > 1 )); then
+    ENCOUNTER_ARRAY=($(generate_array $ENCOUNTER_START $ENCOUNTER_END $ENCOUNTER_DISC))
+    # find last index
+    last_index=$((${#ENCOUNTER_ARRAY[@]} - 1))
+    # Ensure the first and last values are the two desired values
+    ENCOUNTER_ARRAY[0]="$ENCOUNTER_START"
+    ENCOUNTER_ARRAY[$last_index]=$ENCOUNTER_END
+else 
+    ENCOUNTER_ARRAY[0]="$ENCOUNTER_START"
+fi
 
-echo "nonFmort will use the following values: ${NONFMORT_ARRAY[@]}"
+echo "Encounter will use the following values: ${ENCOUNTER_ARRAY[@]}"
 echo ""
-echo "###############################################################################"
+
+###############################################################################
+# GENERATE THE ARRAY OF VALUES TO TRY FOR PARAMETER
+# Generate array
+if (( $K_DISC > 1 )); then 
+    K_ARRAY=($(generate_array $K_START $K_END $K_DISC))
+    # find last index
+    last_index=$((${#K_ARRAY[@]} - 1))
+    # Ensure the first and last values are the two desired values
+    K_ARRAY[0]="$K_START"
+    K_ARRAY[$last_index]=$K_END
+
+    echo "K will use the following values: ${K_ARRAY[@]}"
+    echo ""
+    echo "###############################################################################"
+else
+    K_ARRAY[0]=1
+fi
+
 
 ###############################################################################
 # SETUP nonFmort VARIABLE TO UNIQUE VALUE EACH LOOP
 # CURRENTLY IT WILL STEP BY VALUES OF 0.01
-for i in $(seq 1 $NUM_PARALLEL_DISC); do
-
-    # ADJUST FOR STARTING AND ENDING VALUES.
-    #NONFMORT_VAL=$(bc -l <<< "scale=2; $EXP_REF+(${i}-1)/100")
-    NONFMORT_VAL="${NONFMORT_ARRAY[$i-1]}"
-
-    # echo "The value generated by loop ${i} is ${NONFMORT_VAL}"
-
-    # RUN ON SPECIFIC CPU CORE. START AT 10
-    CPU_CORE=$((i+10))
-    echo "This will run on CPU_CORE ${CPU_CORE}"
-
-    # ACTUAL COMMAND TO RUN THE MULTIYEAR BASH SCRIPT.
-    # & SYBMOL AT THE END MEANS IT WILL NOT WAIT FOR THE PROGRAM TO FINISH BEFORE 
-    # CONTINUING THROUGH THIS LOOP
-    if [ $LOCATION_NAME == "TEST" ]; then
-        echo "TEST -- Running./run_multiyear.sh ${LOCATION_NAME} ${NUM_YEARS} ${NONFMORT_VAL} ${CPU_CORE}"
-	echo ""
-    else
-        ./run_multiyear.sh "${LOCATION_NAME}" "${NUM_YEARS}" "${NONFMORT_VAL}" "${CPU_CORE}"&
-    fi
+CPU_CORE=1
+for i in $(seq 1 $FMORT_DISC); do
     
+    for j in $(seq 1 $ENCOUNTER_DISC); do
+
+        for k in $(seq 1 $K_DISC); do
+
+            # ADJUST FOR STARTING AND ENDING VALUES.
+            #NONFMORT_VAL=$(bc -l <<< "scale=2; $EXP_REF+(${i}-1)/100")
+            NONFMORT_VAL="${NONFMORT_ARRAY[$i-1]}"
+            ENCOUNTER_VAL="${ENCOUNTER_ARRAY[$j-1]}"
+            K_VAL="${K_ARRAY[$k-1]}"
+
+            # echo "The value generated by loop ${i} is ${NONFMORT_VAL}"
+
+            # RUN ON SPECIFIC CPU CORE. START AT 10
+            echo "This will run on CPU_CORE ${CPU_CORE}"
+
+            # ACTUAL COMMAND TO RUN THE MULTIYEAR BASH SCRIPT.
+            # & SYBMOL AT THE END MEANS IT WILL NOT WAIT FOR THE PROGRAM TO FINISH BEFORE 
+            # CONTINUING THROUGH THIS LOOP
+            if [ $LOCATION_NAME == "TEST" ]; then
+                echo "TEST -- Running./run_multiyear.sh ${LOCATION_NAME} ${NUM_YEARS} ${CPU_CORE} ${NONFMORT_VAL} ${ENCOUNTER_VAL} ${K_VAL}"
+        	echo ""
+            else
+                ./run_multiyear.sh "${LOCATION_NAME}" "${NUM_YEARS}" "${CPU_CORE}" "${NONFMORT_VAL}"  "${ENCOUNTER_VAL}" "${K_VAL}"&
+            fi
+            CPU_CORE=$((CPU_CORE + 1 ))
+            
+            if (( $CPU_CORE > 127 )); then
+                echo "!!"
+                echo "Too many cores used...exiting."
+                echo "!!"
+                exit 1
+            fi
+        done
+    done
 done
 
 # WAIT FOR ALL OF THE COMMANDS TO COMPLETE, THIS OVERRULES THE & SYMBOL
