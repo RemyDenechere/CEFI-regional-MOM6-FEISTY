641c6406e869ec144381217ec7b360559e9c3688
Rémy <remy.denechere@gmail.com>
2024-12-12 12:12:20 -0800

    Commit on 2024-12-12 12:12:18

--- builds/rockfish/linux-gnu.mk
+++ builds/rockfish/linux-gnu.mk
@@ -98,7 +98,7 @@ FFLAGS := -fcray-pointer -fdefault-double-8 -fdefault-real-8 -Waliasing -ffree-l
 # Flags based on perforance target (production (OPT), reproduction (REPRO), or debug (DEBUG)
 FFLAGS_OPT = -O3
 FFLAGS_REPRO = -O2 -fbounds-check
-FFLAGS_DEBUG = -O0 -pg -W -fbounds-check -fbacktrace -ffpe-trap=invalid,zero,overflow
+FFLAGS_DEBUG = -O0 -pg -g -W -fbounds-check -fbacktrace -ffpe-trap=invalid,zero,overflow
 
 # Flags to add additional build options
 FFLAGS_OPENMP = -fopenmp
@@ -118,7 +118,7 @@ CFLAGS := -D__IFC
 # Flags based on perforance target (production (OPT), reproduction (REPRO), or debug (DEBUG)
 CFLAGS_OPT = -O2
 CFLAGS_REPRO = -O2
-CFLAGS_DEBUG = -O0 -pg
+CFLAGS_DEBUG = -O0 -g -pg
 
 # Flags to add additional build options
 CFLAGS_OPENMP = -fopenmp
