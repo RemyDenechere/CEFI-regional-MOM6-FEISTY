52655f1c94a45d0afdfecc748831a9becff781ea
Rémy <remy.denechere@gmail.com>
2025-02-04 12:04:49 -0800

    added looping option to run over all the locations in an online and offline settings

---
+++ exps/OM4.single_column.COBALT/run_LOC_offline.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# List of locations
+locations=(BATS CCE GOF NS)
+
+# Loop through each location and run the script
+for loc in "${locations[@]}"; do
+    echo "Running ./run_COBALT_offline.sh for location: $loc"
+    ./run_COBALT_offline.sh "$loc"
+done
+
+echo "All locations processed successfully."
