5d548d4fb4455e7bb7431a4df06a686f256930c1
rdenechere <rdenechere@monkfish>
2025-03-24 13:40:48 -0700

    update param testing

--- exps/OM4.single_column.COBALT/run_LOC_online.sh
+++ exps/OM4.single_column.COBALT/run_LOC_online.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # List of locations
-locations=(GOM) # BATS CCE GOM NS
+locations=(BATS GOM) # BATS CCE GOM NS
 
 # Experiment name
 if [ -z "$1" ]; then
-    exp="FEISTY--non-vertical"
+    exp="FEISTY--non-vertical-refuge-1"
 else
     exp=$1
 fi
 
+value_Rfug=$(echo "10^-10" | bc -l)
+echo "Rfug value:" $value_Rfug
+
 # Loop through each location and run the script
 for loc in "${locations[@]}"; do
-    echo "Running ./parallel_loop.sh " $loc " 1 1 0.1 0.1 1 70 70 1 1 1 1 1 1 " $exp
-    ./parallel_loop.sh $loc 1 1 0.1 0.1 1 70 70 1 1 1 1 1 1 $exp 
+    echo "Running ./parallel_loop.sh " $loc " 20 5 0 0.4 1 70 70 1 1 1 5 $value_Rfug 1 " $exp
+    ./parallel_loop.sh $loc 20 5 0 0.4 1 70 70 1 1 1 5 $value_Rfug 1 $exp 
 done
 
 echo "All locations processed successfully."
